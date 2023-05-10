7bc7fcdb191f81d32937761f023604e913ffc7f5
JJMM22 <112445044+JJMM22@users.noreply.github.com>
2023-05-10 16:18:26 -0600

    Update fire.js

--- fire.js
+++ fire.js
@@ -2,14 +2,19 @@ var firebase = require('firebase')
 // datos de firebase
 var config = {
 
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyAFSMbZ3kbr4i7ITJmJe28xyX0F65eAeSw",
+
+  authDomain: "proyectclima.firebaseapp.com",
+
+  databaseURL: "https://proyectclima-default-rtdb.firebaseio.com",
+
+  projectId: "proyectclima",
+
+  storageBucket: "proyectclima.appspot.com",
+
+  messagingSenderId: "982720757168",
+
+  appId: "1:982720757168:web:b29fc7b7f3ab02e09d2a75"
 };
 
 const fire = firebase.initializeApp(config);
